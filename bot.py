cde8916acab339b47d8ae25955dc782e4927603a
Renedwin2 <84302859+Renedwin2@users.noreply.github.com>
2025-04-18 01:21:44 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 
 import os
 
-BOT_TOKEN = os.getenv("7718024967:AAFeR4uU8dsDnKZOKKuuTOucFXRwdUVkms")
+BOT_TOKEN = os.getenv("AAFeR4uU8dsDnKZOKKuuTOucFXRwdUVkms")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello from Render!")
